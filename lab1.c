44a4fd1f093af2753f3fbfb982b710aebc3c6b57
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2021-05-04 08:59:48 -0700

    Update lab1.c
    introduction of status ints

--- lab1.c
+++ lab1.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
     int pid = fork();
 
     if (pid > 0) {
-        waitpid(pid, status, 0);
+        waitStatus = waitSt(&waitStatus);
+        waitpid(pid, &waitStatus, 0);
         printf(1, "This is a parent process. PID: ", pid);
         exitSt(0);
     } else if (pid < 0) {
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
        printf(1, "This is a child process, PID: ", pid);
    } else if (pid > 0) {
        printf(1, "This is a parent process. PID: ", pid);
-       waitpid(pid, status, 0);
+       waitpid(pid, &waitStatus, 0);
    } else if (pid < 0) {
        printf(1, "Error! No processes");
    }
