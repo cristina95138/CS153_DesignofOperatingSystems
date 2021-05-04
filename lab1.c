e886a2e2bfac359581c09a01523799f13da712a7
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-05-04 08:58:35 -0700

    fixes

--- lab1.c
+++ lab1.c
@@ -8,18 +8,28 @@ int waitPidStatus = 1;
 
 int main(int argc, char *argv[]) {	
     int pid = fork();
-    pid = waitSt(&waitStatus);
-    printf(1, "pid: %d\n", pid);
-    pid = hello();
-    if(waitpid(pid, &waitPidStatus, 0) != -1) {
-        printf(1, "waitPidStatus status: %d\n", waitPidStatus);
-    }
-    printf(1, "waitpid complete.\nwaitSt status: %d\n", waitStatus);
 
-    //exit();
-    printf(1, "\n");
-    exitSt(getpid());
-    printf(1, "exitSt status: %d\n", exitStatus);
+    if (pid > 0) {
+        waitpid(pid, status, 0);
+        printf(1, "This is a parent process. PID: ", pid);
+        exitSt(0);
+    } else if (pid < 0) {
+        printf(1, "Error! No processes");
+    }
+
+    pid = fork();
+
+   if (pid == 0) {
+       printf(1, "This is a child process, PID: ", pid);
+   } else if (pid > 0) {
+       printf(1, "This is a parent process. PID: ", pid);
+       waitpid(pid, status, 0);
+   } else if (pid < 0) {
+       printf(1, "Error! No processes");
+   }
+
+
+    exitSt(0);
 }
 
 
