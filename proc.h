8b522d1c7262f455148b2fb21b1e5997cde28f1b
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2021-05-12 03:46:57 -0700

    lab2 test
    define T_burst.

--- proc.h
+++ proc.h
@@ -53,6 +53,7 @@ struct proc {
   int prior_val;               // Keeps track of the priority value
   int T_start;                 // Start time of turnaround time
   int T_finish;                // Finish time of turnaround time
+  int T_burst;                 // Run time of process
 };
 
 // Process memory is laid out contiguously, low addresses first:
