d33d2c131e60180f587c4f29153af7a6508670aa
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2021-05-18 09:50:20 -0700

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -10,24 +10,29 @@ int main(int argc, char *argv[]) {
     int pid;
     pid = fork();
 
-    //setPrior(1);
-    //int i, k;
+    setPrior(1);
+    int i, k;
 
-    if (!pid) {
-	printf(1, "Error, parent process\n");
-     //   exit();
-     }  else {
-        /*for(i = 0; i < 43000; i++) {
+    if (pid == 0) {
+        for(i = 0; i < 43000; i++) {
             asm("nop");
             for(k=0; k < 43000; k++) {
                 asm("nop");
             }
-        }*/
-	//printf(1, "a");
+        }
         printf(1, "Child %d that has a priority of %d has finished.\n", getpid(), 1);
-	trackSched();
-	//printf(1, "b");
+        trackSched();
+        exit();
+    } else {
+        printf(1, "Error\n");
     }
+
+    if (pid > 0) {
+        wait();
+    }
+
+    exit();
 
     return 0;
 }
+
