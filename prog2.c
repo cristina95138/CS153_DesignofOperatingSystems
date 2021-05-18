0fa70df442b6a3f06fc0d4899c64acf0982f46cc
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2021-05-18 09:51:01 -0700

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
                 asm("nop");
             }
         }
-        printf(1, "Child %d that has a priority of %d has finished.\n", getpid(), 5);
-        trackSched();
+        printf(1, "Child %d that has a priority of %d has finished.\n", getpid(), 5, trackSched());
+        
         exit();
     } else {
         printf(1, "Error\n");
